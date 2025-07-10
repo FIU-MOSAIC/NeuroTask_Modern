6aed231c1ccec6823d68a9b9823d82c077de7450
SamUnderscoreAsd <fssamfarajzadeh@gmail.com>
2025-07-10 15:44:14 -0400

    added some basic classes for the game controller manager

---
+++ lib/games/narration_reading_game/controllers/narration_game_controller.dart
@@ -0,0 +1,35 @@
+import 'package:get/get.dart';
+import 'package:neurotask_ng/core/game/base/game_controller.dart';
+
+class NarrationGameController extends GameController{
+
+  @override
+  void onGameStart(){
+  }
+
+  @override
+  void onGameEnd(){
+
+  }
+
+  @override
+  void onGamePause(){
+
+  }
+
+  @override
+  void onGameResume(){
+
+  }
+
+  @override
+  void onGameReset(){
+
+  }
+
+  @override
+  void onUserInteraction(dynamic data){
+
+  }
+
+}
