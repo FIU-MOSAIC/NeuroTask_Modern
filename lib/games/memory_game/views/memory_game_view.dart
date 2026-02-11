3f5921fc3931a3d28582953f96abe3f07c6edd42
ipbarroso <igorpontebarroso04@gmail.com>
2026-02-11 18:10:10 -0500

    Memory game fixed spacing, changed card colors for better visuals and added a flipping animation

--- lib/games/memory_game/views/memory_game_view.dart
+++ lib/games/memory_game/views/memory_game_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'dart:math';
 import '../../../core/game/widgets/game_view.dart';
 import '../controllers/memory_game_controller.dart';
 import '../models/card_model.dart';
@@ -48,11 +49,11 @@ class MemoryGameView extends StatelessWidget {
       ),
     );
   }
-
+  
   Widget _buildCardGrid() {
     return LayoutBuilder(
       builder: (context, constraints) {
-        double spacing = 12;
+        double spacing = 6;
         int crossAxisCount = 4;
         // childAspectRatio < 1 makes the cards taller than they are wide
         return GridView.builder(
@@ -61,7 +62,7 @@ class MemoryGameView extends StatelessWidget {
             crossAxisCount: crossAxisCount,
             crossAxisSpacing: spacing,
             mainAxisSpacing: spacing,
-            childAspectRatio: 0.65, // Try 0.65 for a tall card look
+            childAspectRatio: 0.51, // Try 0.65 for a tall card look
           ),
           itemCount: controller.cards.length,
           itemBuilder: (context, index) {
@@ -73,30 +74,99 @@ class MemoryGameView extends StatelessWidget {
     );
   }
 
+
   Widget _buildCard(CardModel card) {
     return GestureDetector(
       onTap: () => controller.onUserInteraction(card),
-      child: Obx(() => Card(
-        elevation: 4,
-        child: Container(
-          decoration: BoxDecoration(
-            color: card.isMatched.value
-              ? Colors.green.shade100 
-              : card.isFlipped.value
-                ? Colors.white 
-                : Colors.blue.shade300,
-            borderRadius: BorderRadius.circular(8),
+      child: Obx(()  {
+        return _FlipContainer(
+          isFlipped: card.isFlipped.value || card.isMatched.value,
+          child: Card(
+            elevation: 4,
+            child: Container(
+              decoration: BoxDecoration(
+                color: card.isMatched.value
+                  ? Colors.green.shade300 
+                  : card.isFlipped.value
+                      ? Colors.red.shade300
+                      : Colors.blue.shade300,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Center(
+              child: card.isFlipped.value || card.isMatched.value
+                ? Text(
+                    card.value,
+                    style: TextStyle(fontSize: 64), //emoji size
+                  )
+                : SizedBox.shrink(),
+              ),
+            ),
           ),
-          child: Center(
-            child: card.isFlipped.value || card.isMatched.value
-              ? Text(
-                  card.value,
-                  style: TextStyle(fontSize: 64),
-                )
-              : SizedBox.shrink(),
-          ),
-        ),
-      ),
-    ));
+        );
+      }),
+    );
   }
 }
+
+//this widget add a smooth flip of the card
+  class _FlipContainer extends StatefulWidget {
+    final Widget child;
+    final bool isFlipped;
+
+    const _FlipContainer({required this.child, required this.isFlipped});
+
+    @override
+    State<_FlipContainer> createState() => _FlipContainerState();
+  }
+
+  class _FlipContainerState extends State<_FlipContainer>
+      with SingleTickerProviderStateMixin {
+        late AnimationController _controller;
+
+        @override 
+        void initState(){
+          super.initState();
+          _controller = AnimationController(
+            vsync: this,
+            duration: Duration(milliseconds: 400),
+          );
+          if (widget.isFlipped) _controller.value = 1;
+        }
+
+        @override
+        void didUpdateWidget(covariant _FlipContainer oldWidget) {
+          super.didUpdateWidget(oldWidget);
+          if (widget.isFlipped != oldWidget.isFlipped) {
+            if (widget.isFlipped) {
+              _controller.forward();
+          } else {
+            _controller.reverse();
+      }
+    }
+  }
+
+
+        @override
+        void dispose() {
+          _controller.dispose();
+          super.dispose();
+        }
+
+        @override
+        Widget build(BuildContext context) {
+          return AnimatedBuilder(
+            animation: _controller,
+            builder: (_, child){
+              double angle = _controller.value * pi;
+              if (angle > pi /2) angle = angle - pi;
+
+              return Transform (
+                alignment: Alignment.center,
+                transform: Matrix4.rotationY(angle),
+                child: child,
+              );
+            },
+            child: widget.child,
+          );
+        }
+      }
